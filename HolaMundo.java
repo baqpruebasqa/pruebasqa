2ebfd994ed1c9bcbde3230bba622ad8a041d59c9
baqpruebasqa <75845383+baqpruebasqa@users.noreply.github.com>
2021-02-19 16:26:13 -0500

    Delete HolaMundo.java

--- HolaMundo.java
+++
@@ -1,8 +0,0 @@
-public class HolaMundo {
-	public static void main(String [] args){
-		System.out.println("Hola Mundo");
-		System.out.println("Prueba de Calidad 2");
-		 int pasajeros;  //número
-   		 int capacidad;  //capacidad
-	}
-}
