165216794390d2eea56d4bbab4eb52b666fab930
baqpruebasqa <75845383+baqpruebasqa@users.noreply.github.com>
2021-02-19 16:22:16 -0500

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+	public static void main(String [] args){
+		System.out.println("Hola Mundo");
+		System.out.println("Prueba de Calidad 2");
+		 int pasajeros;  //número
+   		 int capacidad;  //capacidad
+	}
+}
